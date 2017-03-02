0f9da87906678f3e17d233dc760fbd02ec31f63b
mikesuprise1 <mikesupris1@gmail.com>
2017-03-02 13:40:48 -0700

    Pushing additional files

---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { ProposalListComponent } from './proposal/proposal-list.component';
+import { ProposalNewComponent } from './proposal/proposal-new.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	declarations: [
+		AppComponent,
+		HomepageComponent,
+		DocumentsComponent,
+		ProposalListComponent,
+		ProposalNewComponent
+	],
+	bootstrap: [
+		AppComponent
+	]
+})
+export class AppModule {}
