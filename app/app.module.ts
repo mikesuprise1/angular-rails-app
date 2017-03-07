1c331fb33ba167c3dea9c318c798e9fbc82d3bfa
mikesuprise1 <mikesupris1@gmail.com>
2017-03-07 14:04:35 -0700

    Creating a document service

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; 
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { DocumentService } from './documents/document.service';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
 import { ProposalShowComponent } from './proposal/proposal-show.component';
@@ -26,6 +27,9 @@ import { AppRoutingModule } from './app-routing.module';
 		ProposalListComponent,
 		ProposalNewComponent,
 		ProposalShowComponent
+	],
+	providers: [
+		DocumentService
 	],
 	bootstrap: [
 		AppComponent
